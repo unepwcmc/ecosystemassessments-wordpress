e937877b7c2699a021734834f3ce7b730d404348
nym1k <sleeping.stars@gmail.com>
2021-02-25 17:55:37 +0000

    Link tiles component, update to section header mixins, new gradient for card overlays

--- content/themes/ea/blocks/link-tiles/block.php
+++ content/themes/ea/blocks/link-tiles/block.php
@@ -2,52 +2,53 @@
   /*
     Link Tiles Block
     Created by UNEP-WCMC
-    With Block Lab for Gutenberg - https://getblocklab.com/
+    With Genesis Custom Blocks for Gutenberg - https://wordpress.org/plugins/genesis-custom-blocks/
   */
+
+  // Variables
+
+  $section_title = block_field( 'section-title', false );
 ?>
 
-<div class="block-link-tiles">
-  <div class="block-link-tiles__inner">
-    <div class="block-link-tiles__body">
-      <?php if ( block_rows( 'tile' ) ) : ?>
-        <ul class="block-link-tiles__items">
+<div class="link-tiles">
+  <div class="link-tiles__header">
+    <h3 class="link-tiles__title">
+      <?php echo $section_title; ?>
+    </h3>
+  </div>
+  <div class="link-tiles__body">
+    <?php if ( block_rows( 'tile' ) ) : ?>
+      <ul class="link-tiles__items">
 
-          <?php while ( block_rows( 'tile' ) ) : block_row( 'tile' );?>
-            <li class="block-link-tiles__item">
+        <?php while ( block_rows( 'tile' ) ) : block_row( 'tile' );?>
 
-              <div class="block-link-tile">
-                <div class="block-link-tile__image-wrap">
-                  <?php
-                    $attachment_id = block_sub_value( 'image' );
-                    $img_url = wp_get_attachment_image_url( $attachment_id, 'full-size' );
-                  ?>
-                  <img src="<?php echo $img_url; ?>" alt="#" class="block-link-tile__image">
-                </div>
+          <li class="link-tiles__item">
+            <div class="link-tile">
+              <?php if ( block_sub_value( 'title' ) ) : ?>
+                <h4 class="link-tile__title">
+                  <?php echo block_sub_value( 'title' ); ?>
+                </h4>
+              <?php endif; ?>
 
-                <div class="block-link-tile__content">
-                  <?php if ( block_sub_value( 'title' ) ) : ?>
-                    <h3 class="block-link-tile__title"><?php echo block_sub_value( 'title' ); ?></h3>
-                  <?php endif;?>
+              <?php if ( block_sub_value( 'image' ) ) : ?>
+                <?php $image_url = wp_get_attachment_image_src( block_sub_value( 'image' ), 'full-size' )[0]; ?>
+                <img
+                  class="link-tile__image"
+                  src="<?php echo $image_url; ?>"
+                  alt="<?php echo block_sub_value( 'title' ); ?>"
+                >
+                <div class="link-tile__image-overlay"></div>
+              <?php endif; ?>
 
-                  <?php if ( block_sub_value( 'text' ) ) : ?>
-                    <p class="block-link-tile__text"><?php echo block_sub_value( 'text' ); ?></p>
-                  <?php endif;?>
+              <?php if ( block_sub_value( 'link-url' ) ) : ?>
+                <a class="link-tile__fauxlink" href="<?php echo block_sub_value( 'link-url' ); ?>"></a>
+              <?php endif; ?>
+            </div>
+          </li>
 
-                  <?php if ( block_sub_value( 'link-url' ) ) : ?>
-                    <p class="block-link-tile__link">
-                      <?php get_template_part( '/template-parts/icons/icon', 'arrow-right' ); ?>
-                      <?php echo block_sub_value( 'link-text' ); ?>
-                    </p>
-                    <a href="<?php echo block_sub_value( 'link-url' ); ?>" class="block-link-tile__fauxlink" title="<?php echo block_sub_value( 'link-text' ); ?>"></a>
-                  <?php endif;?>
-                </div>
-              </div>
+        <?php endwhile; ?>
 
-            </li>
-          <?php endwhile; ?>
-
-        </ul>
-      <?php endif; reset_block_rows( 'tile' ); ?>
-    </div>
+      </ul>
+    <?php endif; reset_block_rows( 'tile' ); ?>
   </div>
 </div>
--- content/themes/ea/page-resources.php
+++ content/themes/ea/page-resources.php
@@ -36,39 +36,20 @@ get_header(); ?>
 <div class="layout-container">
 	<div class="layout-container__inner">
 		<div class="layout-container__body">
-			<section class="layout-primary layout-primary--restrained">
+			<section class="layout-primary">
 				<div class="layout-primary__body">
-					<h3><a href="/resources/all">All Resources</a></h3>
-					<h3>Stages</h3>
-					<ul>
-						<li>
-							<a href="/resources/resource_stage/introductory-resources">Introductory Resources</a>
-						</li>
-						<li>
-							<a href="/resources/resource_stage/scoping">Scoping</a>
-						</li>
-						<li>
-							<a href="/resources/resource_stage/expert-evaluation">Expert Evaluation</a>
-						</li>
-						<li>
-							<a href="/resources/resource_stage/using-assessments ">Using Assessments </a>
-						</li>
-					</ul>
-					<h3>Types</h3>
-					<ul>
-						<li>
-							<a href="/resources/resource_type/videos-webinars">Videos & Webinars</a>
-						</li>
-						<li>
-							<a href="/resources/resource_type/guides">Guides</a>
-						</li>
-						<li>
-							<a href="/resources/resource_type/case-studies">Case studies</a>
-						</li>
-						<li>
-							<a href="/resources/resource_type/reports">Reports</a>
-						</li>
-					</ul>
+					<?php
+						if ( have_posts() ) :
+							while ( have_posts() ) :
+								the_post();
+
+								get_template_part( 'template-parts/content/content', 'page' );
+
+							endwhile; // End of the loop.
+						else :
+								get_template_part( 'template-parts/content/content', 'none' );
+						endif;
+					?>
 				</div>
 			</section>
 		</div>
