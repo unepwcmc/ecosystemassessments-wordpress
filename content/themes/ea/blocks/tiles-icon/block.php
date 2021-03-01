212f5444656c87509c909950aaddd5f1219e9e10
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-01 13:03:11 +0000

    fix: created a new inner container to apply the inner mixin to it, keep button styling the same at all screen sizes

--- content/themes/ea/blocks/tiles-icon/block.php
+++ content/themes/ea/blocks/tiles-icon/block.php
@@ -1,5 +1,5 @@
 <?php
-  /*
+/*
     Tiles Icon Large
     Created by UNEP-WCMC
     With Genesis Custom Blocks for Gutenberg - https://wordpress.org/plugins/genesis-custom-blocks/
@@ -7,54 +7,57 @@
 ?>
 
 <div class="tiles-icon">
-  <div class="tiles-icon__body">
-    <?php if ( block_rows( 'tile' ) ) : ?>
-      <ul class="tiles-icon__items">
+  <div class="tiles-icon__inner">
+    <div class="tiles-icon__body">
+      <?php if (block_rows('tile')) : ?>
+        <ul class="tiles-icon__items">
 
-        <?php while ( block_rows( 'tile' ) ) : block_row( 'tile' );?>
+          <?php while (block_rows('tile')) : block_row('tile'); ?>
 
-          <li class="tiles-icon__item">
-            <div class="tile-icon">
-              <div class="tile-icon__body">
-                <?php if ( block_sub_value( 'icon' ) ) : ?>
-                  <div class="tile-icon__icon">
-                    <?php $icon_variable = block_sub_value('icon') ?>
-                    <?php get_template_part('template-parts/icons/icon', $icon_variable) ?>
+            <li class="tiles-icon__item">
+              <div class="tile-icon">
+                <div class="tile-icon__body">
+                  <?php if (block_sub_value('icon')) : ?>
+                    <div class="tile-icon__icon">
+                      <?php $icon_variable = block_sub_value('icon') ?>
+                      <?php get_template_part('template-parts/icons/icon', $icon_variable) ?>
+                    </div>
+                  <?php endif; ?>
+
+                  <?php if (block_sub_value('title')) : ?>
+                    <h3 class="tile-icon__title">
+                      <?php echo block_sub_value('title'); ?>
+                    </h3>
+                  <?php endif; ?>
+
+                  <?php if (block_sub_value('description')) : ?>
+                    <p class="tile-icon__description">
+                      <?php echo block_sub_value('description'); ?>
+                    </p>
+                  <?php endif; ?>
+
+                  <div class="tile-icon__links">
+                    <?php if (block_sub_value('link-one')) : ?>
+                      <a class="tile-icon__link" href="<?php echo block_sub_value('link-one'); ?>">
+                        <?php echo block_sub_value('link-one-text'); ?>
+                      </a>
+                    <?php endif; ?>
+
+                    <?php if (block_sub_value('link-two')) : ?>
+                      <a class="tile-icon__link" href="<?php echo block_sub_value('link-two'); ?>">
+                        <?php echo block_sub_value('link-two-text'); ?>
+                      </a>
+                    <?php endif; ?>
                   </div>
-                <?php endif; ?>
-
-                <?php if ( block_sub_value( 'title' ) ) : ?>
-                  <h3 class="tile-icon__title">
-                    <?php echo block_sub_value( 'title' ); ?>
-                  </h3>
-                <?php endif; ?>
-
-                <?php if ( block_sub_value( 'description' ) ) : ?>
-                  <p class="tile-icon__description">
-                    <?php echo block_sub_value( 'description' ); ?>
-                  </p>
-                <?php endif; ?>
-                
-                <div class="tile-icon__links">
-                  <?php if ( block_sub_value( 'link-one' ) ) : ?>
-                    <a class="tile-icon__link" href="<?php echo block_sub_value( 'link-one' ); ?>">
-                      <?php echo block_sub_value('link-one-text'); ?>
-                    </a>
-                  <?php endif; ?>
-
-                  <?php if ( block_sub_value( 'link-two' ) ) : ?>
-                    <a class="tile-icon__link" href="<?php echo block_sub_value( 'link-two' ); ?>">
-                      <?php echo block_sub_value('link-two-text'); ?>
-                    </a>
-                  <?php endif; ?>
                 </div>
-                  </div>
-            </div>
-          </li>
+              </div>
+            </li>
 
-        <?php endwhile; ?>
+          <?php endwhile; ?>
 
-      </ul>
-    <?php endif; reset_block_rows( 'tile' ); ?>
+        </ul>
+      <?php endif;
+      reset_block_rows('tile'); ?>
+    </div>
   </div>
 </div>
