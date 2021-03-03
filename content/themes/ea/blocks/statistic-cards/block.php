9b2c7caf9b00603d3418ae4632e5714acea8160b
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-03 12:46:22 +0000

    fix: reintroduce inner and set dimensions on list item rather than child div within

--- content/themes/ea/blocks/statistic-cards/block.php
+++ content/themes/ea/blocks/statistic-cards/block.php
@@ -7,25 +7,27 @@
 ?>
 
 <div class="statistic-cards">
-  <?php if (block_rows('statistic-card')) : ?>
-    <ul class="statistic-cards__cards">
-      <?php while (block_rows('statistic-card')) : block_row('statistic-card'); ?>
-        <li class="statistic-cards__card">
-          <div class="statistic-card">
-            <?php if (block_sub_value('value')) : ?>
-              <h2 class="statistic-card__value">
-                <?php echo block_sub_value('value'); ?>
-              </h2>
-            <?php endif; ?>
-            <?php if (block_sub_value('statistic')) : ?>
-              <h4 class="statistic-card__statistic">
-                <?php echo block_sub_value('statistic'); ?>
-              </h4>
-            <?php endif; ?>
-          </div>
-        </li>
-      <?php endwhile; ?>
-    </ul>
-  <?php endif;
-  reset_block_rows('tile'); ?>
+  <div class="statistic-cards__inner">
+    <?php if (block_rows('statistic-card')) : ?>
+      <ul class="statistic-cards__cards">
+        <?php while (block_rows('statistic-card')) : block_row('statistic-card'); ?>
+          <li class="statistic-cards__card">
+            <div class="statistic-card">
+              <?php if (block_sub_value('value')) : ?>
+                <h2 class="statistic-card__value">
+                  <?php echo block_sub_value('value'); ?>
+                </h2>
+              <?php endif; ?>
+              <?php if (block_sub_value('statistic')) : ?>
+                <h4 class="statistic-card__statistic">
+                  <?php echo block_sub_value('statistic'); ?>
+                </h4>
+              <?php endif; ?>
+            </div>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    <?php endif;
+    reset_block_rows('tile'); ?>
+  </div>
 </div>
