78b8a546cd62a5487bb979a49078a7414c0acda5
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-03 17:04:27 +0000

    refactor: use an icon wrapper div for the circular background of the icon

--- content/themes/ea/template-parts/components/links/link-view-more.php
+++ content/themes/ea/template-parts/components/links/link-view-more.php
@@ -5,8 +5,10 @@
 <div class="link-view-more">
   <a href="<?php echo $link_args['url']; ?>" class="link-view-more__link">
     <?php echo $link_args['text']; ?>
-    <div class="link-view-more__icon">
-      <?php get_template_part('template-parts/icons/icon', 'angle-right'); ?>
+    <div class="link-view-more__icon-wrapper">
+      <div class="link-view-more__icon">
+        <?php get_template_part('template-parts/icons/icon', 'angle-right'); ?>
+      </div>
     </div>
   </a>
 </div>
