cba8ec109852009ff75ef255b974612efa073409
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-03 13:02:15 +0000

    fix: revert inner, set height on directly on child div and increase padding to resemble design

--- content/themes/ea/blocks/statistic-cards/block.php
+++ content/themes/ea/blocks/statistic-cards/block.php
@@ -7,27 +7,25 @@
 ?>
 
 <div class="statistic-cards">
-  <div class="statistic-cards__inner">
-    <?php if (block_rows('statistic-card')) : ?>
-      <ul class="statistic-cards__cards">
-        <?php while (block_rows('statistic-card')) : block_row('statistic-card'); ?>
-          <li class="statistic-cards__card">
-            <div class="statistic-card">
-              <?php if (block_sub_value('value')) : ?>
-                <h2 class="statistic-card__value">
-                  <?php echo block_sub_value('value'); ?>
-                </h2>
-              <?php endif; ?>
-              <?php if (block_sub_value('statistic')) : ?>
-                <h4 class="statistic-card__statistic">
-                  <?php echo block_sub_value('statistic'); ?>
-                </h4>
-              <?php endif; ?>
-            </div>
-          </li>
-        <?php endwhile; ?>
-      </ul>
-    <?php endif;
-    reset_block_rows('tile'); ?>
-  </div>
+  <?php if (block_rows('statistic-card')) : ?>
+    <ul class="statistic-cards__cards">
+      <?php while (block_rows('statistic-card')) : block_row('statistic-card'); ?>
+        <li class="statistic-cards__card">
+          <div class="statistic-card">
+            <?php if (block_sub_value('value')) : ?>
+              <h2 class="statistic-card__value">
+                <?php echo block_sub_value('value'); ?>
+              </h2>
+            <?php endif; ?>
+            <?php if (block_sub_value('statistic')) : ?>
+              <h4 class="statistic-card__statistic">
+                <?php echo block_sub_value('statistic'); ?>
+              </h4>
+            <?php endif; ?>
+          </div>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+  <?php endif;
+  reset_block_rows('tile'); ?>
 </div>
