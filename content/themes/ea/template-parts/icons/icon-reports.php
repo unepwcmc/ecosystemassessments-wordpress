edca009d19cbe66958ef2f00c15d2b3c4c45c75b
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-04 12:38:29 +0000

    feat: added resource type icons

---
+++ content/themes/ea/template-parts/icons/icon-reports.php
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 294 350">
+  <g data-name="Group 107" transform="translate(-5380 -3245)">
+    <rect data-name="Rectangle 35" width="294" height="350" rx="5" transform="translate(5380 3245)" fill="#57575d" style="mix-blend-mode:multiply;isolation:isolate" />
+    <g data-name="Group 48">
+      <path data-name="Path 473" d="M5475.015 3564.952h-58.8a6.225 6.225 0 01-6.215-6.218v-68.594a6.225 6.225 0 016.217-6.218h43.559v2.665h-43.559a3.557 3.557 0 00-3.553 3.554v68.593a3.556 3.556 0 003.553 3.553h58.8a3.557 3.557 0 003.548-3.553v-53.436h2.665v53.436a6.226 6.226 0 01-6.215 6.218z" fill="#fff" />
+    </g>
+    <g data-name="Group 49">
+      <path data-name="Path 474" d="M5479.902 3506.652h-15.933a5.472 5.472 0 01-5.466-5.465v-15.933a1.333 1.333 0 01.822-1.231 1.33 1.33 0 011.452.29l20.065 20.065a1.332 1.332 0 01.29 1.452 1.332 1.332 0 01-1.23.822zm-18.734-18.182v12.716a2.8 2.8 0 002.8 2.8h12.716z" fill="#fff" />
+    </g>
+    <path data-name="Rectangle 39" fill="#fff" d="M5424.489 3550.623h42.255v2.665h-42.255z" />
+    <path data-name="Rectangle 40" fill="#fff" d="M5424.489 3541.005h42.255v2.665h-42.255z" />
+    <path data-name="Path 475" d="M5466.745 3532.697h-42.256a1.332 1.332 0 01-1.332-1.332v-28.478h2.665v27.146h40.923z" fill="#fff" />
+    <path data-name="Rectangle 41" fill="#fff" d="M5455.02 3518.182h2.665v5.185h-2.665z" />
+    <path data-name="Rectangle 42" fill="#fff" d="M5444.642 3502.886h2.665v20.481h-2.665z" />
+    <path data-name="Rectangle 43" fill="#fff" d="M5434.265 3511.931h2.665v11.436h-2.665z" />
+  </g>
+</svg>
