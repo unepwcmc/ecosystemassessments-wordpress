e5939317f4b42ebc2dc4f114c4dcf0b5bf91a9b4
nym1k <sleeping.stars@gmail.com>
2021-03-11 11:20:45 +0000

    Tweaks to icon link tiles, updated dark grey colour variable, removed backgrounds from resource icon svgs

--- content/themes/ea/blocks/icon-link-tiles/block.php
+++ content/themes/ea/blocks/icon-link-tiles/block.php
@@ -23,23 +23,27 @@
         <?php while ( block_rows( 'tile' ) ) : block_row( 'tile' );?>
 
           <li class="icon-link-tiles__item">
-            <div class="icon-link-tile">
-              <?php if ( block_sub_value( 'title' ) ) : ?>
-                <h4 class="icon-link-tile__title">
-                  <?php echo block_sub_value( 'title' ); ?>
-                </h4>
-              <?php endif; ?>
-              
-              <?php if (block_sub_value('icon')) : ?>
-                <div class="icon-link-tile__image-overlay">
-                  <?php get_template_part('template-parts/icons/icon', block_sub_value('icon')); ?>
-                </div>
-              <?php endif; ?>
 
-              <?php if ( block_sub_value( 'link-url' ) ) : ?>
-                <a class="icon-link-tile__fauxlink" href="<?php echo block_sub_value( 'link-url' ); ?>"></a>
-              <?php endif; ?>
+            <div class="icon-link-tile">
+              <div class="icon-link-tile__content">
+                <?php if ( block_sub_value( 'title' ) ) : ?>
+                  <h4 class="icon-link-tile__title">
+                    <?php echo block_sub_value( 'title' ); ?>
+                  </h4>
+                <?php endif; ?>
+
+                <?php if (block_sub_value('icon')) : ?>
+                  <div class="icon-link-tile__icon">
+                    <?php get_template_part('template-parts/icons/icon', block_sub_value('icon')); ?>
+                  </div>
+                <?php endif; ?>
+
+                <?php if ( block_sub_value( 'link-url' ) ) : ?>
+                  <a class="icon-link-tile__fauxlink" href="<?php echo block_sub_value( 'link-url' ); ?>"></a>
+                <?php endif; ?>
+              </div>
             </div>
+
           </li>
 
         <?php endwhile; ?>
