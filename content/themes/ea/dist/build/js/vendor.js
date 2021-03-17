f680a10f3d3e2db297cad6b36d4828bbfac2ff6c
Stanley Liu <sliu1993@hotmail.co.uk>
2021-03-17 18:06:36 +0000

    feat: implemented and styled custom descriptive cards. moved descriptive card styles into the main stylesheet set of folders to DRY it up, as well as utilising card-listing styles for both sets of cards

--- content/themes/ea/dist/build/js/vendor.js
+++ content/themes/ea/dist/build/js/vendor.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a251daeeed33707ab7e";
+/******/ 	var hotCurrentHash = "817722a2920d2ecba529";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
